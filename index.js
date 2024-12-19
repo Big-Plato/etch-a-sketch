9166dec6e4cf88a9e79dd5b02f6219eecfa51463
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-19 15:05:37 -0300

    Create a button to erase the square of divs, but the button just remove the number of squares divided by two

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 // "use strict";
 
 const container = document.querySelector(".container");
-
 const buttonPrompt = document.createElement("button");
-const squareDiv = document.querySelector(".square-div")
-
-
+const squareOfDivs = document.querySelector(".square-div");
 
 container.appendChild(buttonPrompt);
-
-buttonPrompt.setAttribute("style", "width: 200px;height: 50px; border-radius: 10px; background-color: yellow;")
+buttonPrompt.setAttribute(
+  "style",
+  "width: 200px;height: 50px; border-radius: 10px; background-color: yellow;"
+);
 buttonPrompt.textContent = "Set the number of squares";
 
 let numberSquares;
 
+
+
+// Create the divs based on the input of user
 buttonPrompt.addEventListener("click", () => {
+  // Does not function well yet, but this is to clear the divs created by user input and if he clicks the squares are created based on the new input
 
-    removeSquares();
-    
-    do {
-        numberSquares = prompt("Set a number of squares");
-    } while (numberSquares > 100 || numberSquares < 0)
+  // Asks the user for a number between 0 and 100
+  do {
+    numberSquares = prompt("Set a number of squares");
+  } while (numberSquares > 100 || numberSquares < 0);
 
-    getSquares(numberSquares);
+  getSquares(numberSquares);
+});
 
-})
-
-
-
-const getSquares = (numberSquares) => {
-    for (let i = 0; i < numberSquares; i++) {
-        let divSquare = document.createElement("div");
-        divSquare.className = "square";
-        console.log("div created");
-        squareDiv.appendChild(divSquare);
-        
-    }
-    
-
-
+function getSquares(numberSquares) {
+  for (let i = 0; i < numberSquares; i++) {
+    let divSquare = document.createElement("div");
+    divSquare.className = "square";
+    console.log("div created");
+    squareOfDivs.appendChild(divSquare);
+  }
 }
 
-const removeSquares = () => {
-    for (child of squareDiv.children) {
+const eraseBtn = document.createElement("button");
+container.appendChild(eraseBtn);
+eraseBtn.classList.add("btn");
+eraseBtn.textContent = "Erase";
+
+// The click remove by the number of squares divided by two only, so to clear it up the user have to click many times, I'll fix that somehow
+eraseBtn.addEventListener("click", () => {
+    for (child of squareOfDivs.children) {
         child.remove();
     }
-}
+});
+
+
