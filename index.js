5f3ed665eeea8be892acb634b2e70e63b6fdb7d6
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-19 19:57:04 -0300

    Put everything inside a function. Implement the drawing with mouse hover

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-// "use strict";
+function etchSketch() {
+  const container = document.querySelector(".container");
+  const buttonPrompt = document.createElement("button");
+  const squareOfDivs = document.querySelector(".square-div");
 
-const container = document.querySelector(".container");
-const buttonPrompt = document.createElement("button");
-const squareOfDivs = document.querySelector(".square-div");
-
-container.appendChild(buttonPrompt);
-buttonPrompt.setAttribute(
-  "style",
-  "width: 200px;height: 50px; border-radius: 10px; background-color: yellow;"
-);
-buttonPrompt.textContent = "Set the number of squares";
-
-let numberSquares;
-
-
-
-// Create the divs based on the input of user
-buttonPrompt.addEventListener("click", () => {
-  // Does not function well yet, but this is to clear the divs created by user input and if he clicks the squares are created based on the new input
-
-  // Asks the user for a number between 0 and 100
-  do {
-    numberSquares = prompt("Set a number of squares");
-  } while (numberSquares > 100 || numberSquares < 0);
-
-  getSquares(numberSquares);
-});
-
-function getSquares(numberSquares) {
-  for (let i = 0; i < numberSquares; i++) {
-    let divSquare = document.createElement("div");
-    divSquare.className = "square";
-    console.log("div created");
-    squareOfDivs.appendChild(divSquare);
+  let i = 0;
+  while (i < 32) {
+    let divBase = document.createElement("div");
+    divBase.className = "square";
+    squareOfDivs.appendChild(divBase);
+    i++;
   }
+
+  container.appendChild(buttonPrompt);
+  buttonPrompt.setAttribute(
+    "style",
+    "width: 200px; height: 50px; border-radius: 10px; background-color: yellow;"
+  );
+  buttonPrompt.textContent = "Set the number of squares";
+
+  let numberSquares;
+
+  function setOpacity() {
+    this.setAttribute("style", "background-color: black;");
+  }
+
+  // Create the divs based on the input of user
+  buttonPrompt.addEventListener("click", () => {
+    // Asks the user for a number between 0 and 100
+    do {
+      numberSquares = prompt("Set a number of squares");
+    } while (numberSquares > 100 || numberSquares < 0);
+
+    numberSquares *= 2;
+    getSquares(numberSquares);
+
+    let selectedSquare = document.querySelectorAll(".square");
+    select(selectedSquare);
+  });
+
+  function getSquares(numberSquares) {
+    for (let i = 0; i < numberSquares + numberSquares; i++) {
+      let divSquare = document.createElement("div");
+      divSquare.className = "square";
+      console.log("div created");
+      squareOfDivs.appendChild(divSquare);
+    }
+  }
+
+  
+
+  function select (selectedSquare) {
+  selectedSquare.forEach((square) => {
+    square.addEventListener("mouseover", setOpacity);
+  });
 }
 
-const eraseBtn = document.createElement("button");
-container.appendChild(eraseBtn);
-eraseBtn.classList.add("btn");
-eraseBtn.textContent = "Erase";
+  const eraseBtn = document.createElement("button");
+  container.appendChild(eraseBtn);
+  eraseBtn.classList.add("btn");
+  eraseBtn.textContent = "Erase";
 
-// The click remove by the number of squares divided by two only, so to clear it up the user have to click many times, I'll fix that somehow
-eraseBtn.addEventListener("click", () => {
+  // The click remove by the number of squares divided by two only, so to clear it up the user have to click many times, I'll fix that somehow
+  eraseBtn.addEventListener("click", () => {
     for (child of squareOfDivs.children) {
-        child.remove();
+      child.remove();
     }
-});
+  });
+}
 
-
+etchSketch();
