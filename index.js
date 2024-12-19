9fa69426e07f762942f3bd3309fe66f4aa283559
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-19 14:43:31 -0300

    Create a function to clear the divs

--- index.js
+++ index.js
@@ -3,27 +3,46 @@
 const container = document.querySelector(".container");
 
 const buttonPrompt = document.createElement("button");
-const divSquare = document.createElement("div");
+const squareDiv = document.querySelector(".square-div")
+
 
 
 container.appendChild(buttonPrompt);
 
-buttonPrompt.setAttribute("width: 200px;height: 50px;")
+buttonPrompt.setAttribute("style", "width: 200px;height: 50px; border-radius: 10px; background-color: yellow;")
 buttonPrompt.textContent = "Set the number of squares";
 
 let numberSquares;
 
 buttonPrompt.addEventListener("click", () => {
+
+    removeSquares();
+    
     do {
         numberSquares = prompt("Set a number of squares");
     } while (numberSquares > 100 || numberSquares < 0)
 
-        getSquares(numberSquares);
+    getSquares(numberSquares);
 
 })
 
+
+
 const getSquares = (numberSquares) => {
     for (let i = 0; i < numberSquares; i++) {
-        divSquare;
+        let divSquare = document.createElement("div");
+        divSquare.className = "square";
+        console.log("div created");
+        squareDiv.appendChild(divSquare);
+        
+    }
+    
+
+
+}
+
+const removeSquares = () => {
+    for (child of squareDiv.children) {
+        child.remove();
     }
 }
